a00fdea16bc1c528196c54cc437db056e919d3fb
kimalbab <kimalbab@gmail.com>
2022-06-25 14:02:30 +0900

    generics

---
+++ my_toy_02_self_study_java/02_Java_Recap_Self/src/recap_0625_gnenric/Generics.java
@@ -0,0 +1,74 @@
+package recap_0625_gnenric;
+
+import java.util.ArrayList;
+import java.util.List;
+import static java.util.Collections.*;
+import java.util.*;
+
+public class Generics {
+
+	public static void main(String[] args) {
+		
+	
+		/*Collection Recap
+		List list = new ArrayList();
+		System.out.println(list);
+		
+		addAll(list, 1,2,3,4,5);
+		System.out.println(list);
+		
+		rotate(list,2);
+		System.out.println(list);
+		
+		swap(list, 0, 2);
+		System.out.println(list);
+		
+		shuffle(list);
+		System.out.println(list);
+		
+		sort(list, reverseOrder());
+		System.out.println(list);
+		
+		sort(list);
+		System.out.println(list);
+		
+		int idx = binarySearch(list,3);
+		System.out.println("index of 3 = " + idx);
+		
+		System.out.println("max = " + max(list));
+		System.out.println("min + " + min(list));
+		System.out.println("min = " + max(list, reverseOrder()));
+		
+		
+		fill(list, 9);
+		System.out.println(list);
+		
+		// 리스트와 같은 크기의 새로운 리스트를 생성하고 2로 채운다
+		List newList = nCopies(list.size(),2);
+		System.out.println(newList);
+		
+		System.out.println(disjoint(list, newList));
+		
+		copy(list, newList);
+		System.out.println(list);
+		System.out.println(newList);
+		
+		replaceAll(list, 2, 1);
+		System.out.println(list);
+		
+		Enumeration e = enumeration(list);
+		ArrayList list2 = list(e);
+		
+		System.out.println(list2);
+		*/
+		
+		/* Generics */
+		/* 제네릭을 사용하면 컴파일시 타입체크가 강화된다!
+		 * 지정된 타입 이외의 타입이 들어오지 않게 함.
+		 * 부모클래스를 제네릭으로 설정해주면 부모클래스를 상속받은 자손객체들도 리스트레 담을 수 있음
+		 *  */
+		
+		
+	}
+
+}
